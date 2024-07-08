ddf151c64464fa3c7d578dd4d54e64587be68650
ThanhHoang2002 <thanhhoang11102002@gmail.com>
2024-07-08 10:13:56 +0700

    preparing for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-react-tailwindcss/',
   plugins: [react()],
   server:{
-  }
+  },
+  build:{
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
